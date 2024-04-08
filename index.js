3f3ecbd2f66f3e136560daa1a4ddd8f60a77a7cd
Myungyeon Chloe Lee <49894861+2myemy@users.noreply.github.com>
2024-04-07 21:14:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ fastify.register(fastifyStatic, {
 const start = async () => {
   try {
     await fastify.listen({
-        port: PORT, host: 'http://3.139.151.153'
+        port: PORT, host: '3.139.151.153'
     });
     console.log(`Server running on http://localhost:${PORT}`);
   } catch (err) {
